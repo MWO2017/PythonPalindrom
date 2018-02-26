276b1d307e1e0a8f715fc929f691af5ed1d8647f
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-02-26 23:20:02 +0100

    dzialajacy
    dzialajacy ale nie dla wszystkich

--- palindrom.py
+++ palindrom.py
@@ -1,14 +1,17 @@
 
-#for i in range (1, 201):
+# for i in range (1, 201):
 i = 199
 dana = 0
-kandydat = 199
+kandydat = 196
 if str(kandydat) == str(kandydat)[::-1]:
     print ("Liczba " + str(i) + " jest juz palindromem i nie trzeba sprawdzac hipotezy")
 else:
- while (dana != kandydat):
-     dana = kandydat + int(str(kandydat)[::-1])
-     kandydat = int(str(dana)[::-1])
-     if (dana == kandydat):
-         print ("Dla liczby " + str(i) + " sprawdzono hipoteze i wyliczono PALIDROM " + str(dana))
+    i=0
+    while (dana != kandydat and i<300):
+        dana = kandydat + int(str(kandydat)[::-1])
+        print "dana: "+str(dana)
+        kandydat = int(str(dana)[::-1])
+        if (dana == kandydat):
+            print ("Dla liczby " + str(i) + " sprawdzono hipoteze i wyliczono PALIDROM " + str(dana))
+        i+=1
 
