b730665392e3498df891ffff0b8331b1a07462d3
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-02-27 13:06:36 +0100

    dzialajaca wersja
    przerobiony algorytm i wykluczenie liczby osemkowej

--- palindrom.py
+++ palindrom.py
@@ -1,25 +1,28 @@
+debug=1
 
 for i in range(1, 201):
     print "LICZBA: " + str(i)
     dana = 0
     kandydat = i
     if str(kandydat) == str(kandydat)[::-1]:
-        print ("Liczba " + str(i) + " jest juz palindromem i nie trzeba sprawdzac hipotezy")
+        print ("Liczba " + str(i) + " jest _JUZ_ palindromem i nie trzeba sprawdzac hipotezy")
     else:
         j = 0
-        while (dana != kandydat and j < 20):
-            print "KANDYDAT: " + str(kandydat)
-            tymczasowy = str(kandydat)[::-1]
-            tymczasowy = tymczasowy.lstrip("0")
-            dana = kandydat + int(tymczasowy)
-            print "dana: " + str(dana)
-            tymczasowy2 = str(dana)[::-1]
-            tymczasowy2 = tymczasowy2.lstrip("0")
-            kandydat = int(tymczasowy2)
-            if (dana == kandydat):
-                print ("Dla liczby " + str(i) + " sprawdzono hipoteze i wyliczono PALIDROM " + str(dana))
+        #wyliczamy nowego kandydata wg teorii
+        tymczasowy = int((str(kandydat)[::-1]).strip("0"))
+        nowy_kandydat=kandydat + tymczasowy
+        odwrocony_nowy_kandydat=False
+        #if debug == 1: print str(j+1)+". nowy kandydat: "+str(nowy_kandydat)
+        
+        while (nowy_kandydat != odwrocony_nowy_kandydat and j <30):
+            if debug == 1: print str(j+1)+". nowy kandydat: "+str(nowy_kandydat)
+            odwrocony_nowy_kandydat=int((str(nowy_kandydat)[::-1]).lstrip("0"))
+            if debug == 1: print "odwrocony nowy kandydat: "+str(odwrocony_nowy_kandydat)
+            if nowy_kandydat==odwrocony_nowy_kandydat:
+                print ("Dla liczby " + str(i) + " sprawdzono hipoteze i wyliczono PALIDROM " + str(nowy_kandydat))
                 break
-            j += 1
+            else:
+                nowy_kandydat=nowy_kandydat+odwrocony_nowy_kandydat
+            j+=1
         else:
             print "liczba: " + str(i) + " chyba NIE jest palindromem, bo po " + str(j) + " iteracjach nie znaleziono"
-
